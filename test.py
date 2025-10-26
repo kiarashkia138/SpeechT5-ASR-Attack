795c4abd4d6e2b97d7890eb98ff758363f108941
kiarash kiani <kiarashkia138@gmail.com>
2025-10-26 16:55:53 +0330

    test updated

--- test.py
+++ test.py
@@ -9,5 +9,5 @@ def gen_text(audio_path):
     print(result["text"])
 
 if __name__ == "__main__":
-    gen_text("adversarial_audio3_5dB_cw.wav")
-    gen_text("adversarial_audio3_5dB_pgd.wav")
+    gen_text("adversarial_audio_cw.wav")
+    gen_text("adversarial_audio_pgd.wav")
